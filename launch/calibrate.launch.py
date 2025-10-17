34cd1292eeb50171cfff7a1aa0269b342a179387
hunterhuth <hunterhuth0@gmail.com>
2025-10-17 16:46:25 -0600

    first commit

---
+++ launch/calibrate.launch.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2020-2024, Michael Ferguson
+# All rights reserved.
+#
+# Software License Agreement (BSD License 2.0)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.actions import DeclareLaunchArgument
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import os
+import sys
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("trimming_config"),
+                    "urdf",
+                    "kuka_robot.urdf.xacro",
+                ]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("trimming_config"),
+            "ros2_control",
+            "ros2_controllers.yaml",
+        ]
+    )
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("trimming_config"), "rviz", "view_robot.rviz"]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, robot_controllers],
+        output={
+            
+            'stdout': 'screen',
+            'stderr': 'screen',
+            },
+    )
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_trajectory_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    # Delay rviz start after `joint_state_broadcaster`
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
+        )
+    )
+
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
+
+    # Load the capture config
+    capture_config = os.path.join(
+        get_package_share_directory('kuka_calibration'),
+        'config',
+        'capture.yaml'
+    )
+
+    # Load the calibration config
+    calibration_config = os.path.join(
+        get_package_share_directory('kuka_calibration'),
+        'config',
+        'calibrate.yaml'
+    )
+
+    # Load the calibration poses YAML
+    calibration_poses = os.path.join(
+        get_package_share_directory('kuka_calibration'),
+        'config',
+        'calibration_poses.yaml'
+    )
+
+    # Make a directory for bagfiles to be located
+    try:
+        os.mkdir("/tmp/kuka_calibration")
+    except FileExistsError:
+        pass
+
+    return LaunchDescription([
+        control_node,
+        robot_state_pub_node,
+        joint_state_broadcaster_spawner,
+        robot_controller_spawner,
+        # Turn off auto exposure on camera for better results
+        Node(
+            name='camera_reconfigure',
+            package='ubr1_calibration',
+            executable='camera_reconfigure.py',
+            arguments=['--disable'],
+            output='screen'
+        ),
+        # Calibration
+        Node(
+            name='robot_calibration',
+            package='robot_calibration',
+            executable='calibrate',
+            arguments=[calibration_poses],
+            parameters=[capture_config,
+                        calibration_config],
+            output='screen',
+        ),
+        # Record bagfile for debugging
+        ExecuteProcess(
+            cmd=["ros2", "bag", "record", "/calibration_data", "/robot_description"],
+            cwd="/tmp/kuka_calibration"
+        )
+    ])
